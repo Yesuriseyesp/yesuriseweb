110f287c5e1f5d9242a0ab41f1f4e9190e1c8787
Yesuriseyesp <162588769+Yesuriseyesp@users.noreply.github.com>
2024-03-07 23:37:44 +0530

    Add files via upload

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './About.css'; // Import CSS for styling
 import aboutImage from './about.png'; // Import the PNG image
+import { Link } from 'react-router-dom';
+
 
 const AboutSection = () => {
   return (
@@ -15,8 +17,11 @@ const AboutSection = () => {
       {/* <p>Our mission is to empower startups and small businesses to thrive in the digital landscape. With our expertise and passion for innovation, we aim to revolutionize the way companies engage with their audience and achieve success online.</p> */}
       <p>At Yesurise, we understand the unique challenges of today's digital world. That's why we offer personalized services to meet your specific needs and help you achieve your goals effectively and affordably.</p>
       <p>Join us on this exciting journey as we transform ideas into reality and pave the way for digital success. Let's collaborate and unlock new opportunities for your business together!</p>
-    <button className="button">Contact us</button>
-    
+  
+
+    <Link to="/contact">
+              <button className="button">Reach Us</button> 
+              </Link>    
         </div>
       </div>
     </div>
